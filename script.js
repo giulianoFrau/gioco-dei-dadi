737296f1347f9e0f7e24a5c8cc01c31eef836424
Giuliano <shokteo@gmail.com>
2021-07-05 11:28:59 +0200

    implementazioni

---
+++ script.js
@@ -0,0 +1,89 @@
+'use strict';
+
+//selezione elementi
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1'); //alternativa per prendere elementi con ID,senza #
+const diceEl = document.querySelector('.dice');
+const bntNew = document.querySelector('.btn--new');
+const bntRoll = document.querySelector('.btn--roll');
+const bntHold = document.querySelector('.btn--hold');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+//condizioni iniziali
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  currentScore = 0;
+  player0El.classList.toggle('player--active'); //toogle permette di aggiungere la classe se non esiste e rimuoverla se esiste
+  player1El.classList.toggle('player--active');
+};
+
+let scores = [0, 0]; // tiene i punteggi memorizzati dei 2 giocatori,partono entrambi da 0
+let currentScore = 0; //non puo' essere definito all'interno della funzione perchè ogni volta che facciamo click si riazererebbe
+let activePlayer = 0;
+let playing = true;
+//tirare il dado
+
+bntRoll.addEventListener('click', function () {
+  if (playing) {
+    const dice = Math.trunc(Math.random() * 6) + 1; //genera n° casuali da 1 a 6
+    diceEl.classList.remove('hidden'); // rimuoviamo la classe hidden
+    diceEl.src = `dice-${dice}.png`; //siccome diceEl prende la classe dell img del dado,possiamo con il .src darli un immagine casuale(vedi i nomi delle img)
+
+    if (dice !== 1) {
+      currentScore = currentScore + dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore; //fatto in modo dinamico in modo che possiamo cambiarlo a seconda del giocatore attivo
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+bntHold.addEventListener('click', function () {
+  if (playing) {
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    if (scores[activePlayer] >= 20) {
+        diceEl.classList.add('hidden'); 
+      playing = false;
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+bntNew.addEventListener("click",function(){
+   
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+  
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+  
+    diceEl.classList.add('hidden');
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+    
+})
